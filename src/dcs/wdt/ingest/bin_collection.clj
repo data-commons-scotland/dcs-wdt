7b9bd036d2fb8e9cee2bf10725a095204d6ff2c4
ash-mcc <ashley_mcclenaghan@mac.com>
2022-02-24 10:15:49 +0000

    generate a more detailed CSV file for use by PASI

--- src/dcs/wdt/ingest/bin_collection.clj
+++ src/dcs/wdt/ingest/bin_collection.clj
@@ -4,7 +4,8 @@
             [clojure.java.io :as io]
             [clojure.data.csv :as csv]
             [taoensso.timbre :as log]
-            [dcs.wdt.ingest.shared :as shared]))
+            [dcs.wdt.ingest.shared :as shared])
+  (:import java.time.LocalDate))
 
 (def expected-year-totals {2018 32276.84M
                            2019 32166.32M
@@ -103,12 +104,77 @@
 
 (comment
   
-  (def data0 (db-from-csv-files))
+  ;; ....... BEGIN alt parsing for finer grained data ..................
+
+  ;; a FINER GRAINED alternative to the function that is invoked by ther main WDT functionality
+  (defn finer-grained_customise-map
+    "Converts an externally-oriented map to an internally-oriented map."
+    [m]
+    (let [internal-transfer?-column-label (if (_2021-onwards? m) "Route" "Account")
+          internal-transfer? (contains? internal-transfer-aliases (str/trim (get m internal-transfer?-column-label)))
+          material-column-label (if (_2021-onwards? m) "Waste Collected" "Waste")
+          material (get material-aliases (str/trim (get m material-column-label)))
+          recycling?-column-label (if (_2021-onwards? m) "Category" "Contract")
+          recycling? (contains? recycling-aliases (str/trim (get m recycling?-column-label)))
+          missed-bin? (= "189 Missed Bins" (str/trim (get m "Account")))
+          date (get m "Date")]
+      (if (and (not internal-transfer?)
+               (contains? shared/materials-set material))
+        {:yyyy-MM-dd  (str (subs date 6 10) "-" (subs date 3 5) "-" (subs date 0 2))
+         :HH_mm       (str (subs date 11 13) ":" (subs date 14 16))
+         :route       (get m internal-transfer?-column-label)
+         :quarter     (Integer/parseInt (get m "Quarter"))
+         :material    material
+         :recycling?  recycling?
+         :missed-bin? missed-bin?
+         :tonnes      (bigdec (get m "Quantity"))}
+        (do (log/debugf "Ignoring: %s" m)
+            nil))))
+  
+  ;; a FINER GRAINED alternative to the function that is invoked by ther main WDT functionality
+  (defn finer-grained_csv-file-to-maps
+    "Parses a bin-collection CSV file
+  to return a seq of :bin-collection maps (internal DB records)."
+    [file]
+    (let [year          (Integer/parseInt (second (re-find #"(\d{4})\.csv" (.getName file))))
+          customise-map (partial shared/customise-map finer-grained_customise-map)]
+      (->> file
+           (#(do (log/infof "Reading CSV file: %s" (.getAbsolutePath %)) %))
+           shared/csv-file-to-maps
+           (#(do (log/infof "CSV data rows: %s" (count %)) %))
+           (map customise-map)
+           (remove nil?)
+           (#(do (log/infof "Records to be aggregated: %s" (count %)) %))
+           ;;commented-out: rollup
+           (#(do (log/infof "Accepted records: %s" (count %)) %))
+           (map #(assoc % :year year)))))
+
+  ;; a FINER GRAINED alternative to the function that is invoked by ther main WDT functionality
+  (defn finer-grained_db-from-csv-files []
+    (let [db (->> (str csv-dir "/" (shared/dirname-with-max-supplied-date csv-dir) "/")
+                  shared/find-csv-files
+                  (map finer-grained_csv-file-to-maps)
+                  flatten
+                  (map #(assoc % :record-type :bin-collection
+                               :region "Stirling")))]
+      (when-let [error (shared/check-year-totals :tonnes expected-year-totals db)]
+        (throw (RuntimeException. (format "bin-collection has year-totals error...\nExpected: %s\nActual: %s" (first error) (second error)))))
+      db))
+
+  ;; ....... END alt parsing for finer grained data ..................
+  
+
+  (def data0 (finer-grained_db-from-csv-files))
+
+  (->> data0 (map :tonnes) (apply +)) ;; expect 120,385.2
 
   (pp/print-table (take-last 30 data0))
 
   (def data1 (->> data0
-                  (filter #(= (:recycling? %) true))))
+                  (filter #(= (:recycling? %) true))
+                  (sort-by :yyyy-MM-dd)))
+
+  (->> data1 (map :tonnes) (apply +)) ;; expect 29,311.9
   
   
   ;; ************************ begin PASI related ************************
@@ -117,7 +183,7 @@
   ;;   which can be established by running some of the above code. 
   ;; Take a look at samples of that value...
   
-  (pp/print-table [:year :quarter :material :tonnes]
+  (pp/print-table [:year :quarter :material :tonnes :yyyy-MM-dd :HH_mm :route]
                   (concat (take 5 data1)
                           (take-last 5 data1)))
 
@@ -135,22 +201,13 @@
     (csv/write-csv wtr (cons header-row data-rows)))
   
   ;; write StcilKerbsideRecycling.csv
-  (def header-row ["from" "to" "bin" "batchTonnes"])
-    (defn ->from [{:keys [year quarter]}]
-      (condp = quarter
-        1 (str year "-01-01")
-        2 (str year "-04-01")
-        3 (str year "-07-01")
-        4 (str year "-10-01")
-        :else (throw (ex-info (str "Bad quarter value: " quarter) {}))))
-  (defn ->to [{:keys [year quarter]}]
-      (condp = quarter
-        1 (str year "-04-01")
-        2 (str year "-07-01")
-        3 (str year "-10-01")
-        4 (str (inc year) "-01-01")
-        :else (throw (ex-info (str "Bad quarter value: " quarter) {}))))
-  (def data-rows (map #(vector (->from %) (->to %) (:material %) (:tonnes %)) data1))
+  (def header-row ["from" "to" "HH_mm" "route" "bin" "batchTonnes"])
+  (defn ->to [{:keys [yyyy-MM-dd]}]
+    (-> yyyy-MM-dd
+        LocalDate/parse
+        (.plusDays 1)
+        str))
+  (def data-rows (map #(vector (:yyyy-MM-dd %) (->to %)  (:HH_mm %) (:route %) (:material %) (:tonnes %)) data1))
   (with-open [wtr (io/writer (str pasi-dir "StcilKerbsideRecycling.csv"))]
     (csv/write-csv wtr (cons header-row data-rows)))
 
